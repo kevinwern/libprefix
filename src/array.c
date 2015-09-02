d762082380a995240a55ae6638de98e0eb04f48d
Kevin Wern <kevin.m.wern@gmail.com>
2015-09-02 01:44:17 -0700

    Change casing in array.c to match graph.c
    Change array.c casing to underscore from camel case.
    Fix methods that were declared as static but not implemented as such.
    Remove any functions that have no use for the current implementation of
    prefix-suffix trees.
    Consolidate some functions that performed similar tasks (i.e.
    initialize vs initializing with size) to functions with optional
    parameters.

--- src/array.c
+++ src/array.c
@@ -7,30 +7,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Initialize all array values to empty
-void nullDynArray(DynArray *a){
+//Initialize array, optionally based on size;
+int init_dyn_array(DynArray *a, int size = DEFAULT_SIZE){
   int i;
-  a->size = DEFAULT_SIZE;
-  a->total = 0;
-  a->array = malloc(sizeof(void *) * DEFAULT_SIZE);
-  for (i = 0; i < DEFAULT_SIZE; i++){
-    a->array[i] = NULL;
-  }
-}
-
-//Initialize array based on size
-int initDynArray(DynArray *a, int size){
   a->size = DEFAULT_SIZE;
   while (a->size<size)
     a->size *= 2;
-  a->array = malloc(sizeof(void *) * a->size);
   a->total = 0;
+  a->array = malloc(sizeof(void *) * a->size);
+  for (i = 0; i < DEFAULT_SIZE; i++){
+    a->array[i] = NULL;
+  }
   return 0;
 }
 
-void initDynArrayHashed(DynArray *a){
+void init_dyn_array_hashed(DynArray *a, int size = DEFAULT_SIZE){
   int i;
   a->size = DEFAULT_SIZE;
+  while (a->size<size)
+    a->size *= 2;
   a->array = (void **)malloc(sizeof(void *) * a->size);
   a->total = 0;
   for(i = 0; i < DEFAULT_SIZE; i++)
@@ -39,49 +34,7 @@ void initDynArrayHashed(DynArray *a){
   }
 }
 
-//Initialize array based on initial string
-int initDynArrayStr(DynArray *a, char *str){
-  int size = strlen(str);
-  int i;
-  a->size = DEFAULT_SIZE;
-  while (a->size<size)
-    a->size *= 2;
-  a->array = (void**) malloc(sizeof(void *) * a->size);
-  for (i = 0; i<a->size; i++)
-  {
-    a->array[i] = (void *) malloc(sizeof(char));
-  }
-  a->total = size;
-
-  for(i = 0; i<size; i++){
-    *(char *)((char **)a->array)[i] = str[i];
-  }
-  return 0;
-}
-
-//Insert items into array
-void insertDynArrayStr(DynArray *a, char *str){
-  int addsize = strlen(str);
-  int size = a->size;
-  int i;
-  if (a->array != NULL){
-    while (a->size < a->total+addsize){
-      a->size *= 2;
-    }
-    a->array = (void **) realloc(a->array, sizeof(void *) * a->size);
-
-    for (i = size; i<a->size; i++)
-    {
-      a->array[i] = (void *) malloc(sizeof(char));
-    }
-    for (i = 0; i < addsize; i++){
-      *(char *)((char **)a->array)[i+a->total] = str[i];
-    }
-    a->total = a->total+addsize;
-  }
-}
-
-void insertDynArray(DynArray *a, char c){
+void insert_dyn_array_char(DynArray *a, char c){
   if (a->array != NULL){
     int size = a->size;
     int i;
@@ -98,7 +51,7 @@ void insertDynArray(DynArray *a, char c){
   }
 }
 
-void insertDynArrayNode(DynArray *a, Node *n)
+void insert_dyn_array_node(DynArray *a, Node *n)
 {
   if (a->array != NULL){
     int size = a->size;
@@ -116,7 +69,7 @@ void insertDynArrayNode(DynArray *a, Node *n)
   }
 }
 
-void insertDynArrayNodeHashed(DynArray *a, Node *n)
+void insert_dyn_array_node_hashed(DynArray *a, Node *n)
 {
   if (a->array != NULL){
     int size = a->size;
@@ -131,7 +84,101 @@ void insertDynArrayNodeHashed(DynArray *a, Node *n)
   }
 }
 
-static void rekeyHashedArray(DynArray *a)
+Node *lookup_dyn_array_node_hashed(DynArray *a, char c)
+{
+  if (a->array != NULL){
+    int keyName = c;
+    while (((Node **)a->array)[keyName % a->size] != NULL && ((Node **)a->array)[keyName % a->size]->key != c && keyName != c+a->size){
+      keyName++;
+    }
+    if (((Node **)a->array)[keyName % a->size] != NULL && ((Node *)((Node **)a->array)[keyName % a->size])->key == c){
+        return (Node*)((Node **)a->array)[keyName % a->size];
+    }
+    else
+      return NULL;
+  }
+  return NULL;
+}
+
+void remove_dyn_array_node_hashed(DynArray *a, char c)
+{
+  if (a->array != NULL)
+  {
+    int size = a->size;
+    if (size/2 >= a->total-1 && size > 2)
+    {
+      while (size/2 >= a->total-1 && size > 2){
+	size /= 2;
+      }
+    }
+    findHashKeyAndDoDelete(a, c);
+    a->total -= 1;
+    resizeArray(a, size);
+  }
+}
+
+//Remove items based on size, items removed from end
+void remove_dyn_array(DynArray *a, int remove){
+  int newsize = a->total-remove;
+  if (a->array != NULL && newsize >= 0){
+    while (a->size / 2 > newsize){
+      a->size /= 2;
+    }
+    a->total = newsize;
+    a->array = (void **) realloc(a->array, sizeof(void *) * a->size);
+  }
+}
+
+//returns last character in array
+char pop_dyn_array(DynArray *a){
+  if (a->total == 0) return 0;
+
+  int newsize = a->total-1;
+  if (a->array != NULL && newsize >=0){
+    while (a->size / 2 > newsize){
+      a->size /= 2;
+    }
+    char toReturn = *(char *)((char **)a->array)[a->total-1];
+    a->total = newsize;
+    a->array = realloc(a->array, sizeof(void *) * a->size);
+    return toReturn;
+  }
+  else {
+    return 0;
+  }	  
+}
+
+//Print contents of a DynArray
+void print_dyn_array(DynArray *a){
+  int i;
+  for (i = 0; i < a->total; i++){
+    printf("%c", *(char *)a->array[i]);
+  }
+  printf("\n");
+}
+
+//Convert DynArray to a string
+char *dyn_array_to_str(DynArray *a){
+  char* return_string = malloc(sizeof(char) * (a->total+1));
+  int i;
+  for (i = 0; i < a->total; i++){
+    return_string[i] = *(char *) a->array[i];
+  }
+  return_string[i] = '\0';
+  return return_string;
+}
+
+//Free dynamic array structure
+void clear_dyn_array(DynArray *a){
+  if (a->size != 0){
+    free(a->array);
+    a->total = 0;
+    a->size = 0;
+    a->array = NULL;
+  }
+}
+
+static void rekey_hashed_array(DynArray *a)
 {
   if (a->array != NULL){
     Node **itemsCollected = (Node **) malloc(a->total * sizeof(Node *));
@@ -151,7 +198,7 @@ static void rekeyHashedArray(DynArray *a)
   }
 }
 
-static void findHashKeyAndDoInsert(DynArray *a, Node *n)
+static void find_hash_key_and_do_insert(DynArray *a, Node *n)
 {
   int j = 0, hashKey = n->key % a->size;
   Node **nodeArray = (Node **) a->array;
@@ -176,7 +223,7 @@ static void findHashKeyAndDoInsert(DynArray *a, Node *n)
   }
 }
 
-static void resizeArray(DynArray *a, int size)
+static void resize_array(DynArray *a, int size)
 {
   int i;
   if (size >= a->size)
@@ -201,23 +248,7 @@ static void resizeArray(DynArray *a, int size)
   a->size = size;
 }
 
-Node *lookupDynArrayNodeHashed(DynArray *a, char c)
-{
-  if (a->array != NULL){
-    int keyName = c;
-    while (((Node **)a->array)[keyName % a->size] != NULL && ((Node **)a->array)[keyName % a->size]->key != c && keyName != c+a->size){
-      keyName++;
-    }
-    if (((Node **)a->array)[keyName % a->size] != NULL && ((Node *)((Node **)a->array)[keyName % a->size])->key == c){
-        return (Node*)((Node **)a->array)[keyName % a->size];
-    }
-    else
-      return NULL;
-  }
-  return NULL;
-}
-
-int lookupDynArrayIndexHashed(DynArray *a, char c)
+static int lookup_dyn_array_index_hashed(DynArray *a, char c)
 {
   if (a->array != NULL){
     int keyName = c;
@@ -233,98 +264,10 @@ int lookupDynArrayIndexHashed(DynArray *a, char c)
   return -1;
 }
 
-void removeDynArrayNodeHashed(DynArray *a, char c)
-{
-  if (a->array != NULL)
-  {
-    int size = a->size;
-    if (size/2 >= a->total-1 && size > 2)
-    {
-      while (size/2 >= a->total-1 && size > 2){
-	size /= 2;
-      }
-    }
-//    findHashKeyAndDoDelete(a, c);
-    a->total -= 1;
-    resizeArray(a, size);
-  }
-}
-
-void findHashKeyAndDoDelete(DynArray *a, char c)
+static void find_hash_key_and_do_delete(DynArray *a, char c)
 {
   int indexToDelete = lookupDynArrayIndexHashed(a, c);
   free(a->array[indexToDelete]);
   a->array[indexToDelete] = NULL;
 }
 
-//Remove items based on size, items removed from end
-void removeDynArray(DynArray *a, int remove){
-  int newsize = a->total-remove;
-  if (a->array != NULL && newsize >= 0){
-    while (a->size / 2 > newsize){
-      a->size /= 2;
-    }
-    a->total = newsize;
-    a->array = (void **) realloc(a->array, sizeof(void *) * a->size);
-  }
-}
-
-//returns last character in array
-char popDynArray(DynArray *a){
-  if (a->total == 0) return 0;
-
-  int newsize = a->total-1;
-  if (a->array != NULL && newsize >=0){
-    while (a->size / 2 > newsize){
-      a->size /= 2;
-    }
-    char toReturn = *(char *)((char **)a->array)[a->total-1];
-    a->total = newsize;
-    a->array = realloc(a->array, sizeof(void *) * a->size);
-    return toReturn;
-  }
-  else {
-    return 0;
-  }	  
-}
-
-//Print contents of a DynArray
-void printDynArray(DynArray *a){
-  int i;
-  for (i = 0; i < a->total; i++){
-    printf("%c", *(char *)a->array[i]);
-  }
-  printf("\n");
-}
-
-//Convert DynArray to a string
-char *DynArrayToStr(DynArray *a){
-  char* return_string = malloc(sizeof(char) * (a->total+1));
-  int i;
-  for (i = 0; i < a->total; i++){
-    return_string[i] = *(char *) a->array[i];
-  }
-  return_string[i] = '\0';
-  return return_string;
-}
-
-//Convert DynArray to a string
-char *DynArrayToStrNode(DynArray *a){
-  char* return_string = malloc(sizeof(char) * (a->total+1));
-  int i;
-  for (i = 0; i < a->total; i++){
-    return_string[i] = ((Node *)((Node **) a->array)[i])->key;
-  }
-  return_string[i] = '\0';
-  return return_string;
-}
-
-//Free dynamic array structure
-void clearDynArray(DynArray *a){
-  if (a->size != 0){
-    free(a->array);
-    a->total = 0;
-    a->size = 0;
-    a->array = NULL;
-  }
-}
